4a3b4a189f9a3dc3b7ca284a2e31852f8e778b19
Steven Schmatz <stevenschmatz@gmail.com>
2014-12-19 01:39:17 -0500

    added random

--- random/draw.js
+++ random/draw.js
@@ -2,7 +2,9 @@
 	canvas = setupCanvas();
 	ctx = canvas.getContext("2d");
 
-
+	var interval = setInterval(function() {
+		drawRandomRect(ctx);
+	}, 1)
 
 	document.body.appendChild(canvas);
 }).call(this);
@@ -15,4 +17,22 @@ function setupCanvas() {
 	canvas.height = innerHeight;
 	
 	return canvas;
+}
+
+//MODIFIES: ctx
+//EFFECTS:  Draws a new square in a random location every millisecond
+function drawRandomRect(ctx) {
+	redValue = Math.random() * 255;
+	greenValue = Math.random() * 255;
+	blueValue = Math.random() * 255;
+
+	ctx.fillStyle = "rgb(" + String(parseInt(redValue, 10))
+		 + ", " + String(parseInt(greenValue, 10)) 
+		 + ", " + String(parseInt(blueValue, 10)) 
+		 + ")";
+
+	var x = Math.random() * innerWidth;
+	var y = Math.random() * innerHeight;
+
+	ctx.fillRect(x, y, 50 * Math.random(), 50 * Math.random());
 }
