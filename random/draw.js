df8d0197d696d8dfbec2ab0cda94e97cfc5a8a66
Steven Schmatz <stevenschmatz@gmail.com>
2014-12-19 01:23:08 -0500

    made it a bit clearner

--- random/draw.js
+++ random/draw.js
@@ -1,10 +1,16 @@
-(function() {
-	var canvas = document.createElement("canvas");
-	canvas.width = innerWidth;
-	canvas.height = innerHeight;
-
+(function main() {
+	canvas = setupCanvas();
 	ctx = canvas.getContext("2d");
 
 	document.body.appendChild(canvas);
-
 }).call(this);
+
+
+//EFFECTS: Performs the setup of the HTML5 canvas.
+function setupCanvas() {
+	var canvas = document.createElement("canvas");
+	canvas.width = innerWidth;
+	canvas.height = innerHeight;
+	
+	return canvas;
+}
