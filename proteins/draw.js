0a5abd4966d4f290ffb9ba7162dc4aaf966643a0
Steven Schmatz <stevenschmatz@gmail.com>
2014-12-20 21:07:08 -0500

    upped resolution for proteins

--- proteins/draw.js
+++ proteins/draw.js
@@ -2,17 +2,7 @@
 	canvas = setupCanvas();
 	ctx = canvas.getContext("2d");
 
-	var xPosition = innerWidth/2;
-	var yPosition = innerHeight/2;
-	ctx.moveTo(xPosition, yPosition);
-
-	for (var i = 0; i < 1000; i++) {
-		xPosition += 10*(Math.random() - 0.5);
-		yPosition += 10*(Math.random() - 0.5);
-		ctx.lineTo(xPosition, yPosition);
-	}
-
-	ctx.stroke();
+	drawRandomWalk(ctx);
 
 	document.body.appendChild(canvas);
 }).call(this);
@@ -25,4 +15,21 @@ function setupCanvas() {
 	canvas.height = innerHeight;
 	
 	return canvas;
+}
+
+//MODIFIES: ctx
+//EFFECTS:  Draws a random walk.
+function drawRandomWalk(ctx) {
+
+	var xPosition = innerWidth/2;
+	var yPosition = innerHeight/2;
+	ctx.moveTo(xPosition, yPosition);
+
+	for (var i = 0; i < 50000; i++) {
+		xPosition += 5*(Math.random() - 0.5);
+		yPosition += 5*(Math.random() - 0.5);
+		ctx.lineTo(xPosition, yPosition);
+	}
+
+	ctx.stroke();
 }
